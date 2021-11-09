e7d48abc2b72ab319c77edeecd5148bd52f04901
wentingliu <cindy810317@gmail.com>
2021-11-10 02:10:21 +0800

    Add userController and signup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
+const userController = require('../controllers/userController.js')
 
 module.exports = app => {
   app.get('/', (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', restController.getRestaurants)
   app.get('/admin', (req, res) => res.redirect('/admin/restaurants'))
   app.get('/admin/restaurants', adminController.getRestaurants)
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
 }
