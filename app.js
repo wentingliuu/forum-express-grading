acbfec5868ef6549ba99042e5eef055028352984
wentingliu <cindy810317@gmail.com>
2021-11-09 20:13:49 +0800

    Add restaurant index page

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars()) // Handlebars 註冊樣板引擎
-app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
