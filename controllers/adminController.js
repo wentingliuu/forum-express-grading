3fb197040b8230bbb227137188ee206e1e20de16
wentingliu <cindy810317@gmail.com>
2021-11-16 14:33:13 +0800

    Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -56,10 +56,13 @@ const adminController = {
     }
   },
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true })
-      .then(restaurant => {
-        return res.render('admin/restaurant', { restaurant: restaurant })
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    }).then(restaurant => {
+      return res.render('admin/restaurant', {
+        restaurant: restaurant.toJSON()
       })
+    })
   },
   editRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, { raw: true }).then(restaurant => {
