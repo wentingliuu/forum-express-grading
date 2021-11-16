1cce0c2943e7bc6690a95a24e62384a9c3700e9c
wentingliu <cindy810317@gmail.com>
2021-11-16 16:04:20 +0800

    Add admin categories post feature

--- routes/index.js
+++ routes/index.js
@@ -35,7 +35,9 @@ module.exports = (app, passport) => {
   app.delete('/admin/restaurants/:id', authenticatedAdmin, adminController.deleteRestaurant)
   app.get('/admin/users', authenticatedAdmin, adminController.getUsers)
   app.put('/admin/users/:id/toggleAdmin', authenticatedAdmin, adminController.toggleAdmin)
+
   app.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
+  app.post('/admin/categories', authenticatedAdmin, categoryController.postCategory)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
