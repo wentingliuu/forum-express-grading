65c05fd5b2f4eb1b2539ea11f0b97dad9a51b440
wentingliu <cindy810317@gmail.com>
2021-11-17 15:11:41 +0800

    Create comment model & add association among users, comment, restaurants

---
+++ migrations/20211117065617-create-comment.js
@@ -0,0 +1,36 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' }
+      },
+      RestaurantId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'restaurants', key: 'id' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Comments')
+  }
+}
