2da02e57269ff065f1d61bd65886d4b58a28cd25
wentingliu <cindy810317@gmail.com>
2021-11-21 12:59:53 +0800

    Add add/remove favorite button to restaurant single page

--- controllers/restController.js
+++ controllers/restController.js
@@ -60,13 +60,16 @@ const restController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
+        { model: User, as: 'FavoritedUsers' },
         { model: Comment, include: [User] }
       ]
     })
       .then(restaurant => restaurant.increment('viewCounts'))
       .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.map(d => d.id).includes(req.user.id)
         return res.render('restaurant', {
-          restaurant: restaurant.toJSON()
+          restaurant: restaurant.toJSON(),
+          isFavorited: isFavorited
         })
       })
   },
