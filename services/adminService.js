4ebf805774cd38ce441ad705dc2212780af3b1f9
wentingliu <cindy810317@gmail.com>
2021-11-28 00:55:16 +0800

    PUT /api/admin/restaurants/:id

--- services/adminService.js
+++ services/adminService.js
@@ -57,6 +57,48 @@ const adminService = {
       })
     }
   },
+  putRestaurant: (req, res, cb) => {
+    if (!req.body.name) {
+      return cb({ status: 'error', message: "name didn't exist" })
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID)
+      imgur.upload(file.path, (err, img) => {
+        return Restaurant.findByPk(req.params.id)
+          .then(restaurant => {
+            restaurant.update({
+              name: req.body.name,
+              tel: req.body.tel,
+              address: req.body.address,
+              opening_hours: req.body.opening_hours,
+              description: req.body.description,
+              image: file ? img.data.link : restaurant.image,
+              CategoryId: req.body.categoryId
+            })
+              .then(() => {
+                cb({ status: 'success', message: 'restaurant was successfully to update' })
+              })
+          })
+      })
+    } else {
+      return Restaurant.findByPk(req.params.id)
+        .then(restaurant => {
+          restaurant.update({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: restaurant.image,
+            CategoryId: req.body.categoryId
+          }).then(() => {
+            cb({ status: 'success', message: 'restaurant was successfully to update' })
+          })
+        })
+    }
+  },
   deleteRestaurant: (req, res, cb) => {
     return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
