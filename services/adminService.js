6f860cde8ebd8f21dec106bc417a4d9931421782
wentingliu <cindy810317@gmail.com>
2021-11-27 15:52:23 +0800

    GET /api/admin/restaurants/:id

--- services/adminService.js
+++ services/adminService.js
@@ -11,6 +11,13 @@ const adminService = {
     }).then(restaurants => {
       cb({ restaurants })
     })
+  },
+  getRestaurant: (req, res, cb) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    }).then(restaurant => {
+      cb({ restaurant: restaurant.toJSON() })
+    })
   }
 }
 
